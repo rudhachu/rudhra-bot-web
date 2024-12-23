c9ca5cc6f6a970a857063b5d6e7e9f81a76824db
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-12-23 13:50:38 +0530

    index.js

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
+
+const app = express();
+const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/qrweb',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+app.use(express.static(path.join(__path, 'public')));
+
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__path, '/public/pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(__path, '/public/qr.html'));
+});
+/*
+app.get('/update-session', (req, res) => {
+    res.sendFile(path.join(__path, '/public/updateSession.html'));
+});
+
+let updateSession = require('./updateSession');
+app.use('/sid', updateSession);
+*/
+let qrCode = require('./qr');
+app.use('/qr-code', qrCode);
+
+let pair = require('./pair');
+app.use('/code', pair);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__path, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+});
+
+module.exports = app;
